ee10618ba07115796921ea6f31f56a2e0cf2d4fe
mcuppi <46288829+mcuppi@users.noreply.github.com>
2019-06-21 20:39:01 -0400

    Fix tabs in webpack config

--- webpack.back.config.js
+++ webpack.back.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/backend/app.js',
-    target: 'node',
-    node: {
-        __filename: false,
-        __dirname: false
-    },
-    externals: ['bufferutil', 'utf-8-validate'], // surpress warnings
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.node$/,
-                loader: "native-ext-loader"
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|ico)$/,
-                loader: "file-loader?name=[name].[ext]"
-            },
-        ]
-    },
+	entry: './src/backend/app.js',
+	target: 'node',
+	node: {
+		__filename: false,
+		__dirname: false
+	},
+	externals: ['bufferutil', 'utf-8-validate'], // surpress warnings
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.node$/,
+				loader: "native-ext-loader"
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|ico)$/,
+				loader: "file-loader?name=[name].[ext]"
+			},
+		]
+	},
 };
--- webpack.front.config.js
+++ webpack.front.config.js
@@ -2,45 +2,45 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/frontend/index.js',
-    target: 'web',
-    output: {
-        path: path.resolve(__dirname, 'build/public'),
-        filename: '[name].bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test:/\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    { loader: 'css-loader' },
-                    { loader: 'sass-loader' }
-                ],
-            },
-            {
-                test: /\.(html)$/,
-                loader: "file-loader?name=[name].[ext]"
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|ico)$/,
-                loader: "file-loader?name=/imgs/[name].[ext]",
-                exclude: [/font\.svg$/] // try to exclude svg web fonts
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader?name=/fonts/[name].[ext]"
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({ filename: '[name].bundle.css' }),
-    ]
+	entry: './src/frontend/index.js',
+	target: 'web',
+	output: {
+		path: path.resolve(__dirname, 'build/public'),
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				}
+			},
+			{
+				test:/\.scss$/,
+				use: [
+					{ loader: MiniCssExtractPlugin.loader },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' }
+				],
+			},
+			{
+				test: /\.(html)$/,
+				loader: "file-loader?name=[name].[ext]"
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|ico)$/,
+				loader: "file-loader?name=/imgs/[name].[ext]",
+				exclude: [/font\.svg$/] // try to exclude svg web fonts
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file-loader?name=/fonts/[name].[ext]"
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].bundle.css' }),
+	]
 };
